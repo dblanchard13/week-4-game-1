cc8e64d84b8913eda7376657c551d5a165686db1
dblanchard13 <davidb.prof@gmail.com>
2017-03-07 20:09:09 -0800

    Homework Comments.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,219 +1,95 @@
 $(document).ready(function(){
 
-			var wins = 0;
-			var losses = 0;
-			
-			// This is the sum of the guesses added together. 
-			var counter = 0;
+  var audioElement1 = document.createElement('audio');
+  audioElement1.setAttribute('src', 'https://s3-us-west-1.amazonaws.com/week4sounds/sounds/register.WAV');
 
-			var crystal1 = 0;
-			var crystal2 = 0;
-			var crystal3 = 0;
-			var crystal4 = 0;
+  var audioElement2 = document.createElement('audio');
+  audioElement2.setAttribute('src', 'https://s3-us-west-1.amazonaws.com/week4sounds/sounds/register.mp3');
 
-			var audioElement1 = document.createElement("audio");
-      		audioElement1.setAttribute("src", "https://s3-us-west-1.amazonaws.com/week4sounds/sounds/register.WAV");
+  var song = document.createElement('audio');
+  song.setAttribute('src', 'https://s3-us-west-1.amazonaws.com/week4sounds/sounds/song.mp3');
 
-      		var audioElement2 = document.createElement("audio");
-      		audioElement2.setAttribute("src", "https://s3-us-west-1.amazonaws.com/week4sounds/sounds/register.mp3");
+  var wins = 0;
+  var losses = 0;
 
-      		var song = document.createElement("audio");
-      		song.setAttribute("src", "https://s3-us-west-1.amazonaws.com/week4sounds/sounds/song.mp3");
+  var counter;
+  var numberGuess;
 
+  var crystal1 = $( '#crystal1_span' );
+  var crystal2 = $( '#crystal2_span' );
+  var crystal3 = $( '#crystal3_span' );
+  var crystal4 = $( '#crystal4_span' );
 
-			// This function chooses the random number for each crystal. 
-			function CrystalNumber(crystalName) {
-				return Math.ceil(Math.random() * 12 - 1) + 1;
-			};
 
-			crystal1 = CrystalNumber(crystal1);
-			crystal2 = CrystalNumber(crystal2);
-			crystal3 = CrystalNumber(crystal3);
-			crystal4 = CrystalNumber(crystal4);
-			console.log(crystal1, crystal2, crystal3, crystal4);
+  $('#startButton').on('click', function(){
+    audioElement1.play();
 
-			var numberGuess = Math.floor(Math.random() * 120) + 19;
+    $( '#startButton' ).effect( 'puff' );
+    $( '#crystal1_span' ).effect( 'pulsate' );
+    $( '#crystal2_span' ).effect( 'pulsate' );
+    $( '#crystal3_span' ).effect( 'pulsate' );
+    $( '#crystal4_span' ).effect( 'pulsate' );
+  });
 
-			// Check to see if the numberGuess is working. 
-			console.log(numberGuess);
 
-			// This sends the number the player is trying to reach to the #numberToGuess div.
-			$("#numberToGuess").html(numberGuess);
+  $('.crystal-btn').on('click', function(){
+    $( this ).effect( 'pulsate' )
+    counter += $( this ).data( 'crystal-value' )
+    $( '#playerCounter' ).html( counter );
 
-			// This is the function that controls the game play. 
+    if (counter > numberGuess) {
+      gameLose();
+    } else if (counter === numberGuess) {
+      gameWin();
+    };
 
-			// song.play();
+  });
 
-			function game () {
+  function generateRandomNumber (min, max) {
+    return Math.floor(Math.random() * max) + min;
+  };
 
-				// These show the current score. 
-				$("#playerCounter").html(counter);
-				$("#playerWins").html(wins);
-				$("#playerLosses").html(losses);
+  function setRandomGameValues () {
+    crystal1.data( 'crystal-value', generateRandomNumber(1, 12) );
+    crystal2.data( 'crystal-value', generateRandomNumber(1, 12) );
+    crystal3.data( 'crystal-value', generateRandomNumber(1, 12) );
+    crystal4.data( 'crystal-value', generateRandomNumber(1, 12) );
 
+    counter = 0;
+    $( '#playerCounter' ).html( counter );
 
-				$("#startButton").on("click", function(){
-					// song.play();
-					audioElement1.play();
-					$( "#startButton" ).effect( "puff" );
-					$( "#crystal1_span" ).effect( "pulsate" );
-					$( "#crystal2_span" ).effect( "pulsate" );
-					$( "#crystal3_span" ).effect( "pulsate" );
-					$( "#crystal4_span" ).effect( "pulsate" );
-				});
+    numberGuess = generateRandomNumber(19, 120);
+    $( '#numberToGuess').html( numberGuess );
+  };
 
-				//Crystal #1 function. 
-				$("#crystal1_span").on("click", function(){
-					$( "#crystal1_span" ).effect( "pulsate" );
-					audioElement1.play();
-					counter = counter + crystal1;
-					$("#playerCounter").html(counter);
-					console.log(counter);
+  function gameLose () {
 
-					if (counter > numberGuess) {
-						
-						gameLose();
+    losses ++;
 
-					} else if (counter === numberGuess) {
-							
-						gameWin();
+    $('#playerLosses').html(losses);
 
-					};
+    alert('You lost Crystal Collector!');
 
+    setRandomGameValues();
+  };
 
-				});
+  function gameWin () {
 
-				// Crystal 2 function. 
-				$("#crystal2_span").on("click", function(){
+    song.play();
 
-					audioElement2.play();
-					$( "#crystal2_span" ).effect( "pulsate" );
-					counter = counter + crystal2;
-					$("#playerCounter").html(counter);
-					console.log(counter);
+    wins ++;
 
-					if (counter > numberGuess) {
-						
-						gameLose();
+    $('#playerWins').html(wins);
 
-					} else if (counter === numberGuess) {
-							
-						gameWin();
+    alert('You Win!');
 
-					};
+    setRandomGameValues();
+  };
 
-				});
+  setRandomGameValues();
 
-				// Crystal 3 function. 
-				$("#crystal3_span").on("click", function(){
+  $('#playerCounter').html(counter);
+  $('#playerWins').html(wins);
+  $('#playerLosses').html(losses);
 
-					audioElement1.play();
-					$( "#crystal3_span" ).effect( "pulsate" );
-					counter = counter + crystal3;
-					$("#playerCounter").html(counter);
-					console.log(counter);
-
-					if (counter > numberGuess) {
-						
-						gameLose();
-
-					} else if (counter === numberGuess) {
-							
-						gameWin();
-
-					};
-
-				});
-
-				// Crystal 4 function. 
-				$("#crystal4_span").on("click", function(){
-
-					audioElement2.play();
-					$( "#crystal4_span" ).effect( "pulsate" );
-					counter = counter + crystal4;
-					$("#playerCounter").html(counter);
-					console.log(counter);
-
-					if (counter > numberGuess) {
-						
-						gameLose();
-
-					} else if (counter === numberGuess) {
-							
-						gameWin();
-
-					};
-
-				});
-
-				function gameLose () {
-
-
-					losses = losses + 1;
-
-					$("#playerLosses").html(losses);
-
-					alert("You lost Crystal Collector!");
-
-
-					crystal1 = CrystalNumber(crystal1);
-					crystal2 = CrystalNumber(crystal2);
-					crystal3 = CrystalNumber(crystal3);
-					crystal4 = CrystalNumber(crystal4);
-					console.log(crystal1, crystal2, crystal3, crystal4);
-
-					// This resets the counter back to zero.
-					counter = 0;
-
-					$("#playerCounter").html(counter);
-
-					numberGuess = 0;
-
-					numberGuess = Math.floor(Math.random() * 120) + 19;
-
-					// Check to see if the numberGuess is working. 
-					console.log(numberGuess);
-
-					// This sends the number to guess to the #numberToGuess div
-					$("#numberToGuess").html(numberGuess);
-
-
-				};
-
-
-				function gameWin () {
-
-					song.play();
-
-					wins = wins + 1;
-
-					$("#playerWins").html(wins);
-
-					alert("You Win!");
-
-					crystal1 = CrystalNumber(crystal1);
-					crystal2 = CrystalNumber(crystal2);
-					crystal3 = CrystalNumber(crystal3);
-					crystal4 = CrystalNumber(crystal4);
-					console.log(crystal1, crystal2, crystal3, crystal4);		
-
-					counter = 0;
-
-					$("#playerCounter").html(counter);
-
-					numberGuess = Math.floor(Math.random() * 120) + 19;
-
-					// Check to see if the numberGuess is working. 
-					console.log(numberGuess);
-
-					// This sends the number to guess to the #numberToGuess div
-					$("#numberToGuess").html(numberGuess);
-
-				};
-
-
-			};
-
-			game();
-
-});		
+});
